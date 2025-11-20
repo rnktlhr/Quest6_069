0c22ba2083fcb739e7e9b049829393027906bdec
rnktlhr <rinakitluhur10@gmail.com>
2025-11-20 12:49:04 +0700

    menambah  val status ui pada  viewmodel

--- app/src/main/java/viewmodel/SiswaViewModel.kt
+++ app/src/main/java/viewmodel/SiswaViewModel.kt
@@ -3,10 +3,12 @@ package viewmodel
 import androidx.compose.runtime.MutableState
 import com.example.praktikum6.model.Siswa
 import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
 
 class SiswaViewModel {
     class SiswaViewMode : viewmodel(){
         private val _statusUI = MutableStateFlow(Siswa())
-
+        val statusUI: StateFlow<Siswa> = _statusUI.asStateFlow()
     }
 }
