62a4f9a0ff2499bd82d47cd44c232a43985b0fb8
rnktlhr <rinakitluhur10@gmail.com>
2025-11-20 12:44:45 +0700

    menambah val data jk

--- app/src/main/java/model/DataJK.kt
+++ app/src/main/java/model/DataJK.kt
@@ -1,4 +1,10 @@
-package com.example.myarsitektu
+package com.example.praktikum6.model
+
+import com.example.praktikum6.R
 
 object DataJK {
+    val JenisK = listOf(
+        R.string.laki,
+        R.string.wanita
+    )
 }
