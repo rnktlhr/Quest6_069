c37e10b022c0922368f3c405a372834ea55aeb2a
rnktlhr <rinakitluhur10@gmail.com>
2025-11-20 12:17:09 +0700

    membuat resourses dan libs

---
+++ app/src/main/java/com/example/praktikum6/MainActivity.kt
@@ -0,0 +1,46 @@
+package com.example.praktikum6
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.praktikum6.ui.theme.Praktikum6Theme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            Praktikum6Theme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    DataApp(
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    Praktikum6Theme {
+        Greeting("Android")
+    }
+}
