65bb4e3c4699326711494f0e8d03500496862d50
rnktlhr <rinakitluhur10@gmail.com>
2025-11-20 12:19:06 +0700

    menambah fun pada formulir

--- app/src/main/java/com/example/praktikum6/view/Formulir.kt
+++ app/src/main/java/com/example/praktikum6/view/Formulir.kt
@@ -3,3 +3,13 @@
 package com.example.praktikum6.view
 
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.runtime.Composable
+
+
+@Composable
+fun FormIsian(
+    jenisK: List<String> = listOf("Laki-Laki", "Perempuan"),
+    onSubmitBtnClick : () -> Unit
+){
+
+}
