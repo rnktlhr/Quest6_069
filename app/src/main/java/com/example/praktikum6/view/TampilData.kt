97fe0379d537252cc3c54bfe091eca05635ab6a2
rnktlhr <rinakitluhur10@gmail.com>
2025-11-20 12:29:39 +0700

    menambah val pada tampil data

--- app/src/main/java/com/example/praktikum6/view/TampilData.kt
+++ app/src/main/java/com/example/praktikum6/view/TampilData.kt
@@ -5,3 +5,8 @@ package com.example.praktikum6.view
 fun TampilData(
     onBackBtnClick:()->Unit
 ){
+    val items = listOf(
+        Pair(stringResource(id = R.string.nama_lengkap), "Contoh Nama"),
+        Pair(stringResource(id = R.string.jenis_kelamin), "Lainnya"),
+        Pair(stringResource(id = R.string.alamat), "Yogyakarta")
+    )
