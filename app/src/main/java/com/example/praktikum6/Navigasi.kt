b2377d8723f64ace76e107aff3633e1412fe81e0
rnktlhr <rinakitluhur10@gmail.com>
2025-11-20 13:38:10 +0700

    membenarkan pada bagian navigasi

--- app/src/main/java/com/example/praktikum6/Navigasi.kt
+++ app/src/main/java/com/example/praktikum6/Navigasi.kt
@@ -3,14 +3,20 @@ package com.example.praktikum6
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavController
+import androidx.navigation.NavHost
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import com.example.praktikum6.model.DataJK.JenisK
 import com.example.praktikum6.view.FormIsian
 import com.example.praktikum6.view.TampilData
+import viewmodel.SiswaViewMode
 
 
 enum class Navigasi {
@@ -20,17 +26,23 @@ enum class Navigasi {
 
 @Composable
 fun DataApp(
-    navController: NavHostController = rememberNavController(),
-    modifier: Modifier
+    modifier: Modifier,
+    viewMode: SiswaViewMode = viewModel(),
+    navController: NavHostController = rememberNavController()
 ){
     Scaffold { isiRuang ->
+        val uiState = viewMode.statusUI.collectAsState()
         NavHost(
             navController = navController,
             startDestination = Navigasi.Formulirku.name,
+
             modifier = Modifier.padding(isiRuang)){
             composable(route = Navigasi.Formulirku.name){
+                val konteks = LocalContext.current
                 FormIsian(
+                    PilihanJK = JenisK.map{id -> konteks.resources.getString(id)},
                     onSubmitBtnClick = {
+                        viewMode.setSiswa(it)
                         navController.navigate(Navigasi.Detail.name)
                     }
                 )
