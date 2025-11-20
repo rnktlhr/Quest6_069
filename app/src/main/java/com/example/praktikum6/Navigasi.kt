1b3a8b48960e603db3158ca977ddb4642b8a8058
rnktlhr <rinakitluhur10@gmail.com>
2025-11-20 12:37:49 +0700

    menambah import pada navigasi

--- app/src/main/java/com/example/praktikum6/Navigasi.kt
+++ app/src/main/java/com/example/praktikum6/Navigasi.kt
@@ -1,5 +1,18 @@
 package com.example.praktikum6
 
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavController
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.praktikum6.view.FormIsian
+import com.example.praktikum6.view.TampilData
+
+
 enum class Navigasi {
     Formulirku,
     Detail
