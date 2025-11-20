99923ed6e3086a1d446a6c338661e6eb667f11a3
rnktlhr <rinakitluhur10@gmail.com>
2025-11-20 12:47:55 +0700

    menambah private val viewmodel

--- app/src/main/java/viewmodel/SiswaViewModel.kt
+++ app/src/main/java/viewmodel/SiswaViewModel.kt
@@ -1,7 +1,12 @@
 package viewmodel
 
+import androidx.compose.runtime.MutableState
+import com.example.praktikum6.model.Siswa
+import kotlinx.coroutines.flow.MutableStateFlow
+
 class SiswaViewModel {
     class SiswaViewMode : viewmodel(){
+        private val _statusUI = MutableStateFlow(Siswa())
 
     }
 }
